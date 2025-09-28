3df2b34a1735b19fe55b4b47c06af333bbb3812b
ForgeDom <maxstill2006@gmail.com>
2025-09-28 23:55:11 +0300

    fixed

--- database/seeders/CarSeeder.php
+++ database/seeders/CarSeeder.php
@@ -9,15 +9,6 @@ class CarSeeder extends Seeder
 {
     public function run(): void
     {
-        $car = Car::create([
-            'brand' => 'BMW',
-            'model' => 'X5',
-            'year' => 2022,
-            'price' => 60000,
-            'description' => 'Спортивний SUV',
-        ]);
-
-        $car->photos()->create(['photo_path' => 'cars/bmw_x5_1.jpg']);
-        $car->photos()->create(['photo_path' => 'cars/bmw_x5_2.jpg']);
+      
     }
 }
